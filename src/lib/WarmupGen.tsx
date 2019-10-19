f6e69da1c3c0a1f091af578ff03ce07c022655d2
Jeff Lynn <4128043+jefffm@users.noreply.github.com>
2019-10-18 19:45:01 -0700

    add starting strength warmup

--- src/lib/WarmupGen.tsx
+++ src/lib/WarmupGen.tsx
@@ -15,6 +15,39 @@ export abstract class WarmupGen {
 }
 
 /**
+ * The Starting Strength Warmup
+ * 
+ * 2×5 with just the bar
+ * 1×5 with bar + 25% of weight to add
+ * 1×3 with bar + 50% of weight to add
+ * 1×2 with bar + 75% of weight to add
+ * 3×5 at full working weight
+ */
+export class StartingStrengthWarmupGen extends WarmupGen {
+    mainExercises: IMainExercisesState
+
+    constructor(mainExercises: IMainExercisesState) {
+        super()
+        this.mainExercises = mainExercises
+    }
+
+    getSets(exercise: string, trainingMax: number, firstSetWeight: number): StaticSet[] {
+        const getWeight = (pct: number): number => (
+            45 + ((firstSetWeight - 45) * pct)
+        )
+
+        return [
+            { "reps": 5, "weight": 45 },
+            { "reps": 5, "weight": 45 },
+            { "reps": 5, "weight": getWeight(0.25) },
+            { "reps": 3, "weight": getWeight(0.50) },
+            { "reps": 2, "weight": getWeight(0.75) },
+        ]
+    }
+
+}
+
+/**
  * This class implements the warmup from Beyond 531:
  * 
  * * Bar set for 10 reps
@@ -41,12 +74,7 @@ export class BeyondWarmupGen extends WarmupGen {
         const baseWeight = this.getBaseWeight(exercise)
 
         // Initialize with a bar set
-        var sets: StaticSet[] = [
-            {
-                "reps": 10,
-                "weight": 45
-            }
-        ]
+        var sets: StaticSet[] = [{ "reps": 10, "weight": 45 }]
 
         // Step forward with 10% of your training max each set
         const step = trainingMax * 0.1
@@ -63,12 +91,7 @@ export class BeyondWarmupGen extends WarmupGen {
             for (var i = numSets; i > 0; i--) {
                 const nextWarmupWeight = round5(firstSetWeight - (i * step))
 
-                sets.push(
-                    {
-                        "reps": 5,
-                        "weight": nextWarmupWeight
-                    }
-                )
+                sets.push({ "reps": 5, "weight": nextWarmupWeight })
             }
         }
 
